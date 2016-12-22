3ec53f3f6f812b1407292d541623768e5171f7e4
José Cabezas <jcabezas@bluk.cl>
2016-12-22 11:20:16 -0300

    add images, css styles, and component bootstrap

---
+++ demo.js
@@ -0,0 +1,12 @@
+$(function(){
+  /*$('.toc').affix({
+    offset: {
+      top: 50,
+
+    }
+  });*/
+
+});
+
+$('.bs-component [data-toggle="popover"]').popover();
+$('.bs-component [data-toggle="tooltip"]').tooltip();
